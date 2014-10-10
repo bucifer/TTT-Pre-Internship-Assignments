49c829bcaf08d415dd3a8f45612319b5809639e8
Terry Bu <ktbucifer@gmail.com>
2014-10-10 16:59:50 -0400

    Trying another attempt to refactor out DAO methods

---
+++ Week 4 - 6. NavController Core Data Version/NavController Core Data Version/DAOManager.h
@@ -0,0 +1,13 @@
+//
+//  DAOManager.h
+//  NavController Core Data Version
+//
+//  Created by Aditya Narayan on 10/10/14.
+//  Copyright (c) 2014 NM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DAOManager : NSObject
+
+@end
---
+++ Week 4 - 6. NavController Core Data Version/NavController Core Data Version/DAOManager.m
@@ -0,0 +1,13 @@
+//
+//  DAOManager.m
+//  NavController Core Data Version
+//
+//  Created by Aditya Narayan on 10/10/14.
+//  Copyright (c) 2014 NM. All rights reserved.
+//
+
+#import "DAOManager.h"
+
+@implementation DAOManager
+
+@end
