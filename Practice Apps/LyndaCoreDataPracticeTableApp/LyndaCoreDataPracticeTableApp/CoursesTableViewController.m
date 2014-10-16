c03d9a60cc630ebd0f8e31243a6175e04113beb7
Terry Bu <ktbucifer@gmail.com>
2014-10-16 16:16:14 -0400

    added delete functionality

--- Practice Apps/LyndaCoreDataPracticeTableApp/LyndaCoreDataPracticeTableApp/CoursesTableViewController.m
+++ Practice Apps/LyndaCoreDataPracticeTableApp/LyndaCoreDataPracticeTableApp/CoursesTableViewController.m
@@ -21,8 +21,8 @@
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+//     Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+     self.navigationItem.leftBarButtonItem = self.editButtonItem;
     
     
     NSError *error = nil;
@@ -200,17 +200,23 @@
 }
 */
 
-/*
-// Override to support editing the table view.
+
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+        
+        NSManagedObjectContext *context = self.managedObjectContext;
+        Course *courseToDelete = [self.fetchedResultsController objectAtIndexPath:indexPath];
+        [context deleteObject:courseToDelete];
+        
+        NSError *error = nil;
+        if (![context save:&error]) {
+            NSLog(@"Error: %@", error);
+        }
+        
+    }
 }
-*/
+
 
 /*
 // Override to support rearranging the table view.
