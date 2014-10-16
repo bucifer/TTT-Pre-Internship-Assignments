320c4c2b76a759242009744e6bff3f608717c487
Terry Bu <ktbucifer@gmail.com>
2014-10-16 16:47:19 -0400

    Got dynamic date value working

--- Practice Apps/LyndaCoreDataPracticeTableApp/LyndaCoreDataPracticeTableApp/Course.m
+++ Practice Apps/LyndaCoreDataPracticeTableApp/LyndaCoreDataPracticeTableApp/Course.m
@@ -15,4 +15,13 @@
 @dynamic releaseDate;
 @dynamic title;
 
+
+//this allows you to set the default value for something in the model as soon as it gets initiated
+//instead of using the Defautl Value option in the model file, you cn do this in code, to allow for "dynamic" values
+- (void) awakeFromInsert {
+    [super awakeFromInsert];
+    self.releaseDate = [NSDate date];
+    
+}
+
 @end
